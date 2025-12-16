308c3f237a374b393989ebfb302d79ea2720b214
lwmacct <lwmacct@icloud.com>
2025-12-17 02:18:02 +0800

    chore 2025-12-17 02:18:02 CST

--- internal/command/command.go
+++
@@ -1,7 +0,0 @@
-// Package command 提供客户端和服务端的命令行功能。
-package command
-
-import "github.com/lwmacct/251207-task-remote/internal/config"
-
-// Defaults 默认配置 - 单一来源 (Single Source of Truth)
-var Defaults = config.DefaultConfig()
